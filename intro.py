aca0bd8562151bb582d5733d032047c9dcaffb2a
John Johnson <johnsonj@yahoo.com>
2024-06-16 13:26:35 -0500

    updates

--- intro.py
+++ intro.py
@@ -1,3 +1,18 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 st.write("Hello World!")
+st.title("SAP SAM Log Parser")
+st.header("Header")
+st.text("general text")
+st.markdown("**bold text** *italic text*")
+st.markdown("---")
+st.metric(label="speed", value=100, delta=10)
+
+d = {'col1': [1, 2], 'col2': [3, 4]}
+df = pd.DataFrame(data=d)
+st.table(df)
+st.dataframe(df)
+
+st.checkbox( )
